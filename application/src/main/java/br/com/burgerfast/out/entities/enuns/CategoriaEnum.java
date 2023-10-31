fe0131e06f5b302fed5c4f1530405efa94ef5f11
SDWeb <SDWeb>
2023-10-30 22:58:21 -0300

    Implementação de enum de categoria

--- application/src/main/java/br/com/burgerfast/out/entities/enuns/CategoriaEnum.java
+++ application/src/main/java/br/com/burgerfast/out/entities/enuns/CategoriaEnum.java
@@ -1,4 +1,35 @@
 package br.com.burgerfast.out.entities.enuns;
 
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
 public enum CategoriaEnum {
+    LANCHE(1,"lanche"),
+    ACOMPANHAMENTO(2,"acompanhamento"),
+    BEBIDA(3,"bebida"),
+    SOBREMESA(4,"sobremesa");
+
+    private Integer code;
+    private String value;
+
+
+    private CategoriaEnum(Integer code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public static CategoriaEnum toEnum(String value) {
+        if(value == null) {
+            return null;
+        }
+
+        for(CategoriaEnum x : CategoriaEnum.values()) {
+            if(value.equals(x.getValue())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Value inválido: " + value);
+    }
 }
