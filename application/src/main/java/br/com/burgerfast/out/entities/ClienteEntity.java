bbb3a7e20fd674707899ccfc39b09d297cbbc42c
Primola Oliveira <primola.oliveira@vortigo.com.br>
2023-10-30 22:54:19 -0300

    criação cliente

---
+++ application/src/main/java/br/com/burgerfast/out/entities/ClienteEntity.java
@@ -0,0 +1,29 @@
+package br.com.burgerfast.out.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity(name="cliente")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteEntity {
+
+    @Id
+    private Long id;
+
+    @Column(name="nr_cpf")
+    private Integer cpf;
+
+    @Column(name="nm_cliente")
+    private String name;
+
+    @Column(name="ds_email")
+    private String email;
+}
---
+++ application/src/main/java/br/com/burgerfast/out/entities/enuns/StatusEnum.java
@@ -0,0 +1,4 @@
+package br.com.burgerfast.out.entities.enuns;
+
+public enum StatusEnum {
+}
