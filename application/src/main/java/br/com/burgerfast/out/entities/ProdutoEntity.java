61cf568f10a12f016698a832c05205bbdcd9e263
SDWeb <SDWeb>
2023-10-30 22:49:01 -0300

    Implementação da inteface da tabela de produtos

--- application/src/main/java/br/com/burgerfast/out/entities/ProdutoEntity.java
+++ application/src/main/java/br/com/burgerfast/out/entities/ProdutoEntity.java
@@ -1,11 +1,15 @@
 package br.com.burgerfast.out.entities;
 
+import br.com.burgerfast.out.entities.enuns.CategoriaEnum;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @Builder
@@ -16,5 +20,15 @@ public class ProdutoEntity {
 
     @Id
     private Long id;
-    private
+    @Column(name="nm_produto")
+    private String nome;
+    @Column(name="nm_produto")
+    private CategoriaEnum categoria;
+    @Column(name="vl_preco")
+    private BigDecimal preco;
+    @Column(name="ds_produto")
+    private String descricao;
+    @Column(name="ds_imagem")
+    private String imagem;
+
 }
