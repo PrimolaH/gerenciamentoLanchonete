2bfce42a7bab75b790c525bc07a2cd4efa3ac71b
Primola Oliveira <primola.oliveira@vortigo.com.br>
2023-10-30 23:06:29 -0300

    criação enum status

--- application/src/main/java/br/com/burgerfast/out/entities/enuns/StatusEnum.java
+++ application/src/main/java/br/com/burgerfast/out/entities/enuns/StatusEnum.java
@@ -1,4 +1,33 @@
 package br.com.burgerfast.out.entities.enuns;
 
+import lombok.Getter;
+
+@Getter
 public enum StatusEnum {
+    RECEBIDO(1, "Recebido"),
+    EM_PREPARACAO(2, "Em Preparação"),
+    PRONTO(3, "Pronto"),
+    FINALIZADO(4,"Finalizado");
+
+    private Integer code;
+    private String value;
+
+    private StatusEnum(Integer code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public static StatusEnum toEnum(String value) {
+        if(value == null) {
+            return null;
+        }
+
+        for(StatusEnum x : StatusEnum.values()) {
+            if(value.equals(x.getValue())) {
+                return x;
+            }
+        }
+
+        throw new IllegalArgumentException("Value inválido: " + value);
+    }
 }
