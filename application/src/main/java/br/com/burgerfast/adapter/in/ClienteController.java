48f1a7442d5d185d30196611805cc104e9a0dd46
Primola Oliveira <primola.oliveira@vortigo.com.br>
2023-10-30 23:25:56 -0300

    adapter criação

---
+++ application/src/main/java/br/com/burgerfast/adapter/in/ClienteController.java
@@ -0,0 +1,15 @@
+package br.com.burgerfast.adapter.in;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("cliente")
+public class ClienteController {
+
+    public void buscarCpf(){
+
+    }
+
+    public void cadastroCliente(){
+
+    }
+}
