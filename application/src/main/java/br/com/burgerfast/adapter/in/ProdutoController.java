b7239148fe7dc2e115d824ddea671c23bba6cd18
SDWeb <SDWeb>
2023-11-01 00:04:45 -0300

    implementação ProdutoController

--- application/src/main/java/br/com/burgerfast/adapter/in/ProdutoController.java
+++ application/src/main/java/br/com/burgerfast/adapter/in/ProdutoController.java
@@ -1,15 +1,22 @@
 package br.com.burgerfast.adapter.in;
 
+import br.com.burgerfast.adapter.in.httpModels.ProdutoHttpModel;
+import br.com.burgerfast.adapter.mapper.ProdutoMapper;
+import br.com.burgerfast.core.domain.Produto;
 import br.com.burgerfast.core.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
 public class ProdutoController {
 
     private final ProdutoService produtoService;
+    private final ProdutoMapper produtoMapper;
 
     @GetMapping("/produto/{categoria}")
     public ResponseEntity<String> listaProdutosPorCategoria(@PathVariable String categoria){
@@ -18,20 +25,26 @@ public class ProdutoController {
     }
 
     @PostMapping("/produto")
-    public ResponseEntity<Void> addProduto(){
-        produtoService.addProduto();
+    public ResponseEntity<Void> addProduto(@RequestBody ProdutoHttpModel httpModel){
+        Produto produto = produtoMapper.httpModelTo(httpModel);
+        ProdutoHttpModel produtoHttpModel = produtoMapper.httpModelFrom(produtoService.addProduto(produto));
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id_produto}")
+                .buildAndExpand(produtoHttpModel.getId())
+                .toUri();
         return ResponseEntity.status(201).build();
     }
 
-    @DeleteMapping("/produto")
-    public ResponseEntity<Void> removerProduto(){
-        produtoService.removerProduto();
-        return ResponseEntity.status(204).build();
+    @DeleteMapping("/produto/{id_produto}")
+    public ResponseEntity<Void> removerProduto(@PathVariable Long idProduto){
+        produtoService.removerProduto(idProduto);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/produto")
-    public ResponseEntity<Void> editarProduto(){
-        produtoService.editarProduto();
-        return ResponseEntity.status(204).build();
+    public ResponseEntity<Void> editarProduto(@RequestBody ProdutoHttpModel httpModel){
+        produtoService.editarProduto(produtoMapper.httpModelTo(httpModel));
+        return ResponseEntity.noContent().build();
     }
 }
--- application/src/main/java/br/com/burgerfast/core/service/ProdutoService.java
+++ application/src/main/java/br/com/burgerfast/core/service/ProdutoService.java
@@ -1,22 +1,29 @@
 package br.com.burgerfast.core.service;
 
+import br.com.burgerfast.core.domain.Produto;
+import br.com.burgerfast.core.port.ProdutoPort;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProdutoService{
+
+    private final ProdutoPort produtoPort;
+
     public String listaProdutosPorCategoria(String categoria) {
         return null;
     }
 
-    public void addProduto() {
-
+    public Produto addProduto(Produto produto) {
+        return produtoPort.addProduto(produto);
     }
 
-    public void removerProduto() {
-
+    public void removerProduto(Long idProduto) {
+        produtoPort.removerProduto(Produto.builder().id(idProduto).build());
     }
 
-    public void editarProduto() {
-
+    public void editarProduto(Produto produto) {
+        produtoPort.editarProduto(produto);
     }
 }
